17dac170332300c4ec4a2cb8875071c4d0400086
loipi879 <loitdgcc200345@fpt.edu.vn>
2022-07-18 20:58:01 +0700

    get and add product

--- src/controllers/productCtrl.ts
+++ src/controllers/productCtrl.ts
@@ -1,14 +1,45 @@
 import express from 'express';
+import Products from '../models/productModel';
 
 const productCtr = {
   getProducts: async (req, res) => {
-    res.json({ msg: 'Get all products.' });
+    try {
+      const products = await Products.find();
+
+      res.status(200).json({ products });
+    } catch (err) {
+      res.status(500).json({ msg: err.message });
+    }
   },
   getProduct: async (req, res) => {
-    res.json({ msg: `Get one product by id ${req.params.id}` });
+    try {
+      const product = await Products.findById(req.params.id);
+
+      if (!product) {
+        return res.status(404).json({ msg: 'Product not exist' });
+      }
+      res.status(200).json({ product });
+    } catch (err) {
+      res.status(500).json({ msg: err.message });
+    }
   },
   addProduct: async (req, res) => {
-    res.json({ msg: 'Add new product' });
+    try {
+      const { title, price, description, category, image } = req.body;
+
+      const newProduct = new Products({
+        title,
+        price,
+        description,
+        category,
+        image,
+      });
+      await newProduct.save();
+
+      return res.status(200).json(newProduct);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
   },
   updateProduct: async (req, res) => {
     res.json({ msg: `Update product by id ${req.params.id}` });
