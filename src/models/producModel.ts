07fd60de39ed39586a8b337c39d9d8891d2b5a69
loipi879 <loitdgcc200345@fpt.edu.vn>
2022-07-20 21:41:58 +0700

    Paginate, Sort, Search & Filter (2)

--- src/models/producModel.ts
+++ src/models/producModel.ts
@@ -1,34 +1,37 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
   },
-  image: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-})
+);
 
-productSchema.index({title: 'text'})
+productSchema.index({ title: 'text' });
 
-const Products = mongoose.model('Products', productSchema)
+const Products = mongoose.model('Products', productSchema);
 
-Products.createIndexes({title: 'text'})
+// Products.createIndexes({ title: 'text' });
 
 export default Products;
